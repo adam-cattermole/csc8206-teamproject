62a3027dde2a44d0329921d588137b9fa8faec8d
Justas Miknys (UG) <b0428165@newcastle.ac.uk>
2016-02-25 14:44:14 +0000

    UI changes

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -16,6 +16,7 @@ import ui.utilities.*;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import ui.utilities.GridRectangle;
 
 public class Controller implements Initializable {
 
@@ -26,118 +27,31 @@ public class Controller implements Initializable {
 
     @FXML private ScrollPane scrollPane;
 
-    public final int GRID_HEIGHT = 150;
-    public final int GRID_WIDTH = 150;
-    public final int CELL_SIZE = 30;
+
+    public final int GRID_HEIGHT = 50;
+    public final int GRID_WIDTH = 50;
+    public static final int CELL_SIZE = 30;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Rectangle[][] rectangles = new Rectangle[GRID_WIDTH][GRID_HEIGHT];
+        GridRectangle[][] rectangles = new GridRectangle[GRID_WIDTH][GRID_HEIGHT];
         for (int i = 0; i < GRID_WIDTH; i++) {
             for (int j = 0; j < GRID_HEIGHT; j++) {
-                Rectangle r1 = new Rectangle(i*CELL_SIZE, j*CELL_SIZE, CELL_SIZE, CELL_SIZE);
-                r1.setStroke(Color.DARKBLUE);
-                r1.setStrokeType(StrokeType.INSIDE);
-                r1.setStrokeWidth(0.5);
-                r1.setFill(Color.LIGHTGRAY);
+                GridRectangle r1 = new GridRectangle(i*CELL_SIZE, j*CELL_SIZE, CELL_SIZE, CELL_SIZE, grid, rectangles);
                 rectangles[i][j] = r1;
                 grid.getChildren().add(r1);
-                addGridListener(r1);
+                //addGridListener(r1);
             }
         }
+
         System.out.println(grid.getChildren().size());
         palette.setVgap(5);
+
         palette.setPrefSize(60, 60);
         for (Block b: blockList) {
             addPaletteListener(b);
         }
         scrollPane.setMaxSize(750, 750);
-    }
-
-    private void addGridListener(final Rectangle target) {
-        target.setOnDragOver(new EventHandler<DragEvent>() {
-            public void handle(DragEvent event) {
-                /* data is dragged over the target */
-                System.out.println("onDragOver");
-
-                /* accept it only if it is  not dragged from the same node
-                 * and if it has a string data */
-                if (event.getGestureSource() != target &&
-                        event.getDragboard().hasString()) {
-                    /* allow for both copying and moving, whatever user chooses */
-                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-                }
-
-                event.consume();
-            }
-        });
-
-        target.setOnDragEntered(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                /* the drag-and-drop gesture entered the target */
-                System.out.println("onDragEntered");
-                /* show to the user that it is an actual gesture target */
-                if (event.getGestureSource() != target &&
-                        event.getDragboard().hasString()) {
-                    target.setFill(Color.GREEN);
-                }
-
-                event.consume();
-            }
-        });
-
-        target.setOnDragExited(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                /* mouse moved away, remove the graphical cues */
-                target.setFill(Color.LIGHTGRAY);
-
-                event.consume();
-            }
-        });
-
-        target.setOnDragDropped(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                /* data dropped */
-                System.out.println("onDragDropped");
-                /* if there is a string data on dragboard, read it and use it */
-                Dragboard db = event.getDragboard();
-                boolean success = false;
-                if (db.hasString()) {
-                    String type = db.getString();
-                    Block block;
-                    switch(type) {
-                        case "Section":
-                            block = new Section(target.getX(), target.getY());
-                            break;
-                        case "PointForward":
-                            block = new PointForward(target.getX(), target.getY());
-                            break;
-                        case "PointBackward":
-                            block = new PointBackward(target.getX(), target.getY());
-                            break;
-                        case "PointForwardU":
-                            block = new PointForwardU(target.getX(), target.getY());
-                            break;
-                        case "PointBackwardU":
-                            block = new PointBackwardU(target.getX(), target.getY());
-                            break;
-                        default:
-                            block = null;
-                    }
-                    if (block != null) {
-                        // Here we handle block with the backend
-                        grid.getChildren().add(block);
-                    }
-
-                    success = true;
-                }
-                /* let the source know whether the string was successfully
-                 * transferred and used */
-                event.setDropCompleted(success);
-
-                event.consume();
-            }
-        });
     }
 
     private void addPaletteListener(final ImageView source) {
--- src/ui/utilities/Block.java
+++ src/ui/utilities/Block.java
@@ -1,7 +1,9 @@
 package ui.utilities;
 
+import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 /**
  * Created by Adam Cattermole
@@ -10,14 +12,29 @@ import javafx.scene.image.ImageView;
 
 public abstract class Block extends ImageView {
 
+    boolean selected = false;
+    
     public Block(Image image) {
         super(image);
+        addBlockListeners();
     }
 
     public Block(double x, double y, Image image) {
-        super(image);
+        this(image);
         setX(x);
         setY(y);
+    }
+    
+    private void addBlockListeners()
+    {
+        setOnMouseClicked(new EventHandler <MouseEvent>() 
+        {
+            public void handle(MouseEvent event) 
+            {
+                System.out.println("onMouseClicked");
+                event.consume();
+            }
+        });
     }
 
 }
