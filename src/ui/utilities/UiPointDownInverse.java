2414da42a64fe7bd1ca0760ef1fbe2fe5dc60b67
Adam Cattermole <a.d.cattermole@gmail.com>
2016-03-01 14:54:34 +0000

    UI: adjusted to use canvas instead of images

--- src/ui/utilities/UiPointDownInverse.java
+++ src/ui/utilities/UiPointDownInverse.java
@@ -2,7 +2,9 @@ package ui.utilities;
 
 import backend.Block;
 import backend.Point;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 /**
  * Created by Adam Cattermole
@@ -10,19 +12,31 @@ import javafx.scene.image.Image;
  */
 public class UiPointDownInverse extends UiBlock {
 
-    private static Image image = new Image("ui/resources/point_down_inverse.png");
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 60;
 
     public UiPointDownInverse() {
-        super(image);
+        super(WIDTH, HEIGHT);
     }
 
     public UiPointDownInverse(double x, double y) {
-        super(x, y, image);
+        super(x, y, WIDTH, HEIGHT);
         block = new Point(Point.Orientation.DOWN);
     }
     
     public UiPointDownInverse(double x, double y, Block block) {
-    	super(x, y, image);
+    	super(x, y, WIDTH, HEIGHT);
     	this.block = block;
+    }
+
+    @Override
+    protected void draw() {
+        GraphicsContext gc = getGraphicsContext2D();
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(STROKE_SIZE);
+        gc.strokeLine(0, HEIGHT*.25, WIDTH, HEIGHT*.25);
+        gc.bezierCurveTo(WIDTH*.75, HEIGHT*.25, WIDTH*.5, HEIGHT*.25, WIDTH*.5, HEIGHT*.5);
+        gc.bezierCurveTo(WIDTH*.5, HEIGHT*.5, WIDTH*.5, HEIGHT*.75, 0, HEIGHT*.75);
+        gc.stroke();
     }
 }
