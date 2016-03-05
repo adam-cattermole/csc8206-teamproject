81d88df2a2c239ebb81699e6adaf251a14900513
kubatek94 <j.gawron@newcastle.ac.uk>
2016-03-05 14:38:24 +0000

    Updated Route class

--- src/route/RouteBuilder.java
+++ src/route/RouteBuilder.java
@@ -4,60 +4,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 import backend.Block;
-import backend.Point;
 import backend.Section;
 import backend.Signal;
 
 /**
- * 
+ *
  * @author chris_000
  *
- * This class produces objects that can be used to form the 
+ * This class produces objects that can be used to form the
  * final parameter for the Route constructor over time
- * 
+ *
  * Once the path has been completed, there is a build method
- * which then creates the Route itself, returning it. 
- * 
+ * which then creates the Route itself, returning it.
+ *
  * The RouteBuilder object resets after this build call
  */
 public class RouteBuilder
 {
 	private static int ROUTE_NO = 1;
-	List<Block> path = new ArrayList<Block>();
+	private List<Block> path = new ArrayList<Block>();
 
 	public int addToRoute(Block b)
 	{
 		path.add(b);
 		return path.size();
 	}
-	
+
 	public int addToRoute(List<Block> blocks)
 	{
 		path.addAll(blocks);
 		return path.size();
 	}
-	
+
 	public List<Block> getCurrentPath()
 	{
 		return new ArrayList<Block>(path);
 	}
-	
+
 	public Route build()
 	{
-		Route r = new Route(path);
-		path = new ArrayList<Block>();
-		return r;
+		return new Route(path);
 	}
-	
+
 	public class Route
 	{
-		private final String routeID;
-		private final List<Block> sequence;
-		private final Block start;
-		private final Block end;
+		private final String id;
+
+		private final List<Block> path;
+		private final Section startBlock;
+		private final Section endBlock;
+
+		private final Signal source;
+		private final Signal destination;
+
 		private final List<String> points = new ArrayList<String>();
 		private final List<String> signals = new ArrayList<String>();
-		
+		private final List<String> conflicts = new ArrayList<String>();
+
 		private Route(List<Block> path)
 		{
 			if(path.size() < 2)
@@ -65,30 +68,83 @@ public class RouteBuilder
 				throw new IllegalArgumentException("Insufficient blocks for a route");
 			}
 			
-			routeID = "R" + ROUTE_NO++;
-			start = path.get(0);
-			end = path.get(path.size() - 1);
+			Block start = path.get(0);
+			Block end = path.get(path.size()-1);
 			
-			sequence = path;//TODO Defensive copying; make programming easier too
+			if (!(start instanceof Section && end instanceof Section)) {
+				throw new IllegalArgumentException("First and last block of a route must be a section");
+			}
+
+			id = "R" + ROUTE_NO++;
+			this.path = path;//TODO Defensive copying; make programming easier too
 			
+			startBlock = (Section) path.get(0);
+			endBlock = (Section) path.get(path.size() - 1);
+			
+			//detect the direction based on next block
+			if (startBlock.getUp().equals(path.get(1))) {
+				//we are going in up direction
+				source = startBlock.getSignalUp();
+				destination = endBlock.getSignalUp();
+			} else {
+				//we are going in down direction
+				source = startBlock.getSignalDown();
+				destination = endBlock.getSignalDown();
+			}
 		}
-		
+
 		public String getId() {
-			return routeID;
+			return id;
 		}
 		
-		public Block getStart()
+		public List<Block> getPath()
 		{
-			return start;
+			return path;
 		}
-		
-		public Block getEnd()
+
+		public Block getStartBlock()
 		{
-			return end;
+			return startBlock;
+		}
+
+		public Block getEndBlock()
+		{
+			return endBlock;
 		}
 		
+		public Signal getSource()
+		{
+			return source;
+		}
+		
+		public Signal getDestination()
+		{
+			return destination;
+		}
+		
+		public List<String> getPoints()
+		{
+			return points;
+		}
+		
+		public List<String> getSignals()
+		{
+			return signals;
+		}
+		
+		public List<String> getConflicts()
+		{
+			return conflicts;
+		}
+		
+		public void calculateSettings()
+		{
+			//calculate interlocking settings here
+		}
+
 		public boolean validateRoute()
 		{
+			//validate route based on conflicts
 			return true;
 		}
 	}
